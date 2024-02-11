40ddedea916da7a367e7460524bd13f56e021cd1
Awwlie <104977788+awwliedacoder@users.noreply.github.com>
2024-02-11 16:11:24 +0200

    Create Day 6: 1108. Defanging an IP Address.py

---
+++ Week 1/Day 6: 1108. Defanging an IP Address.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def defangIPaddr(self, address):
+        """
+        :type address: str
+        :rtype: str
+        """
+        newadrs = address.replace('.','[.]')
+        return newadrs
+
+## Easier work today. Feeling hopeful.
+## Might try using TypeScript for the next problem.
+## Wont use these easy easy ones next though. 
